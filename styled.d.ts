030d1d31fd104852e8bc796a36052143671c86df
Henrique Marques <marquesprogrammer@hotmail.com>
2021-05-29 10:01:10 -0300

    updated pallete colors

--- styled.d.ts
+++ styled.d.ts
@@ -7,12 +7,17 @@ declare module 'styled-components' {
     spacing: {
       borderRadius: number;
       border: number;
+      padding: number;
+      margin: number;
     };
     colors: {
       primary: string;
       secondary: string;
       white: string;
       black: string;
+      green: string;
+      grayLight: string;
+      success: string;
     };
     typography: {
       FONT_REGULAR: string;
--- theme.ts
+++ theme.ts
@@ -5,12 +5,17 @@ const myTheme: DefaultTheme = {
   spacing: {
     borderRadius: spacing.RADIUS,
     border: spacing.BORDER,
+    margin: spacing.MARGIN,
+    padding: spacing.PADDING,
   },
   colors: {
     primary: colors.PRIMARY,
     secondary: colors.SECONDARY,
     black: colors.BLACK,
     white: colors.WHITE,
+    green: colors.GREEN,
+    grayLight: colors.GRAY_LIGHT,
+    success: colors.SUCCESS,
   },
   typography: {
     FONT_REGULAR: typography.FONT_FAMILY_REGULAR,
