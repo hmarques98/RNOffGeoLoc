0c8ab7d70458669c2c4d6d3db9fab53c35c03224
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-03 20:31:33 -0300

    chore: using useKeysPackage in screen

--- src/screens/Status.tsx
+++ src/screens/Status.tsx
@@ -1,58 +1,45 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { Box } from 'components/molecules/Box';
 import { Typography } from 'components/molecules/Typography';
 import { StackScreenProps } from '@react-navigation/stack';
 import { CommonStackParamList } from 'src/screens';
 import useHeader from 'hooks/useHeader';
 import { Divider } from 'components/molecules/Divider';
-import useReactQuery from 'hooks/useReactQuery';
-import { log } from '@utils/console';
-import { getPointsById } from '@store/slices/location';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { locationStateSelector } from '@store/slices';
 
 import { FlatList } from 'react-native-gesture-handler';
+import useKeysPackage from 'hooks/useKeysPackage';
 type Props = StackScreenProps<CommonStackParamList, 'Status'>;
 
 const StatusScreen = ({ navigation, route }: Props) => {
   useHeader(route.name);
+  useKeysPackage();
 
-  const dispatch = useDispatch();
-  const { points } = useSelector(locationStateSelector);
-
-  const { data } = useReactQuery<{ keys: string[] }>({
-    path: 'points',
-    queryName: 'points',
-  });
-
-  const getPoints = useCallback(async () => {
-    data?.keys.map(async (item) => {
-      try {
-        dispatch(getPointsById(item));
-      } catch (error) {
-        log({ error });
-      }
-    });
-  }, [data?.keys, dispatch]);
-
-  useEffect(() => {
-    getPoints();
-  }, [getPoints]);
+  const { points, keys, loadedAll } = useSelector(locationStateSelector);
 
   return (
     <Box flex={1} py={3} bg={'white'} px={3}>
-      <FlatList
-        data={points.slice().sort((a, b) => Number(b.time) - Number(a.time))}
-        renderItem={({ item }) => (
-          <CardPoint
-            time={item.time}
-            key={item.id}
-            id={String(item.id)}
-            completed={Boolean(data?.keys.includes(item.id))}
-          />
-        )}
-        ListFooterComponent={() => <Divider my={3} />}
-      />
+      {loadedAll && (
+        <FlatList
+          ListEmptyComponent={() => (
+            <Typography>Nenhum pacote encontrado</Typography>
+          )}
+          showsVerticalScrollIndicator={false}
+          data={points
+            .slice()
+            .sort((a, b) => Number(b?.time) - Number(a?.time))}
+          renderItem={({ item }) => (
+            <CardPoint
+              time={item.time}
+              key={item.id}
+              id={String(item.id)}
+              completed={Boolean(keys.includes(item.id))}
+            />
+          )}
+          ListFooterComponent={() => <Divider my={3} />}
+        />
+      )}
     </Box>
   );
 };
